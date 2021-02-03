11bb04c61d4986d41643044a1701d1fc5e637db1
Kevin Thibedeau <kevin.thibedeau@gmail.com>
2021-02-02 21:18:18 -0500

    Add arg check for HTML sync script

--- doc/sync_html.sh
+++ doc/sync_html.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-rsync -r --delete --exclude=.git --prune-empty-dirs -v _build/html/ $1
+if [ $# -lt 1 ]; then
+  echo "Usage: ${0##*/} <gh-pages path>"
+  exit 1
+fi
+
+html_dir=$1
+
+
+rsync -r --delete --exclude=.git --prune-empty-dirs -v _build/html/ $html_dir
